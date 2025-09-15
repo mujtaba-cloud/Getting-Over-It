15060641b38bf98e2b5b5342acd323997d4f2da3
Faisal <you@example.com>
2025-09-15 20:06:47 +0500

    first commit

---
+++ Java Script/script.js
@@ -0,0 +1,12 @@
+function goFullscreen() {
+    let elem = document.documentElement; // full page
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    }
+}
